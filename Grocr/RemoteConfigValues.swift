3be98bc832cd2a059436fb225c30d56ad21257a2
xianlinbox <xianlinbox@gmail.com>
2018-05-22 22:04:30 -0500

    add default avlues

---
+++ Grocr/RemoteConfigValues.swift
@@ -0,0 +1,19 @@
+//
+//  RCValues.swift
+//  Grocr
+//
+//  Created by Xianning Liu  on 2018/5/22.
+//  Copyright © 2018 Razeware LLC. All rights reserved.
+//
+
+class RemoteConfigValues {
+  static sharedInstance = RemoteConfigValues()
+  
+  init() {
+    loadValues()
+  }
+  
+  private func loadValues() {
+    
+  }
+}
